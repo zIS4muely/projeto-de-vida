0060f6eb3471b2dbd026b00bb1373e44bfe49104
zIS4muely <106540014+zIS4muely@users.noreply.github.com>
2024-03-19 16:17:19 -0300

    at

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
 const botoes=document.querySelectorAll(".botao");
-console.log(botoes);
+
+for(let i=0; i <botoes.length;i++) {
+    botoes[i].onclick= function()  {
+        for(let j=0; j<botoes.length;j++){
+            botoes[j].classList.remove("ativo");
+        }
+        botoes[i].classList.add("ativo");
+    }
+}
