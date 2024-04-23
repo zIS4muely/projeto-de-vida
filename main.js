f70d91bb6089e5f4f1b25b37aa8dc1d140a1730e
zIS4muely <106540014+zIS4muely@users.noreply.github.com>
2024-04-23 16:20:47 -0300

    at

--- main.js
+++ main.js
@@ -15,8 +15,19 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+const tempoObjetivo1 = new Date("2024-06-20T00:00:00");
+const tempoObjetivo2 = new Date("2024-06-12T00:00:00");
+const tempoObjetivo3 = new Date("2024-12-25T00:00:00");
+const tempoObjetivo4 = new Date("2024-11-25T00:00:00");
 
+const tempos= [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
+
+function atualizaCronometro(){
+
+    for (let i=0; i<contadores.length;i++){
+        contadores[i].textContent = calculaTempo(tempos[i]);
+    }
+}
 
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
 
